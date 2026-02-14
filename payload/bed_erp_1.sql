bcd4562289bd1b968e156fb2a4230097326ddf01
tehnplk <tehnplk@gmail.com>
2026-02-14 13:16:03 +0700

    refactor: simplify bed_erp_1 query by using iptadm table instead of subquery
    - Removed SET variable for hoscode, now using inline subquery in SELECT
    - Replaced complex subquery for last_bedno with direct join to iptadm table
    - Changed from lm.last_bedno to a.bedno for cleaner column reference
    - Simplified query structure while maintaining same output

--- payload/bed_erp_1.sql
+++ payload/bed_erp_1.sql
@@ -1,10 +1,8 @@
 -- hos ส่งข้อมูล --
-SET @hoscode = (SELECT hospitalcode FROM opdconfig LIMIT 1);
-
 SELECT
-  @hoscode AS hoscode,
+  (SELECT hospitalcode FROM opdconfig LIMIT 1) AS hoscode,
   CONCAT(LEFT(i.an, 3), REPEAT('X', GREATEST(CHAR_LENGTH(i.an) - 5, 0)), RIGHT(i.an, 2)) AS an_censored,
-  lm.last_bedno AS bedno,
+  a.bedno,
   b.export_code,
   i.regdate,
   i.dchdate,
@@ -16,24 +14,8 @@ SELECT
     ELSE 0
   END AS overlap_days
 FROM ipt i
-JOIN (
-  SELECT
-    i2.an,
-    (
-      SELECT bm.nbedno
-      FROM iptbedmove bm
-      WHERE bm.an = i2.an
-        AND bm.nbedno IS NOT NULL
-      ORDER BY bm.movedate DESC, bm.movetime DESC, bm.iptbedmove_id DESC
-      LIMIT 1
-    ) AS last_bedno
-  FROM ipt i2
-  WHERE i2.regdate IS NOT NULL
-    AND i2.dchdate IS NOT NULL
-    AND i2.regdate <= CURDATE()
-    AND i2.dchdate >= DATE('2025-01-01')
-) lm ON lm.an = i.an
-JOIN bedno b ON b.bedno = lm.last_bedno
+JOIN iptadm a ON a.an = i.an
+JOIN bedno b ON b.bedno = a.bedno
 WHERE i.regdate IS NOT NULL
   AND i.dchdate IS NOT NULL
   AND i.regdate <= CURDATE()
