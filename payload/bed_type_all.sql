1108a9e003ded9bc79dd0ac95e8230824e2c2f24
tehnplk <tehnplk@gmail.com>
2026-02-15 17:12:48 +0700

    chore: remove bed_erp SQL query files
    - Deleted bed_erp_1.sql (hospital data submission query)
    - Deleted bed_erp_2.sql (webapp bed occupancy rate calculation query)

---
+++ payload/bed_type_all.sql
@@ -0,0 +1,5 @@
+SELECT (SELECT hospitalcode FROM opdconfig LIMIT 1) AS hoscode,b.export_code, b.bedno,bedtype,roomno
+FROM bedno b
+WHERE b.export_code IS NOT NULL
+  AND TRIM(b.export_code) <> ''
+  AND CHAR_LENGTH(TRIM(b.export_code)) = 6;
